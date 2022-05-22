79d572ae9fa759dda47c1713d028d0aa2d936850
Haechan Lee <dlgockssla@gmail.com>
2022-05-22 21:49:25 +0900

    git init

---
+++ app/src/main/java/com/example/walkholic/MyAPI.java
@@ -0,0 +1,16 @@
+package com.example.walkholic;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+
+public interface MyAPI {
+
+    @POST("/auth/google")
+    Call<ResponseBody> post_posts(
+            @Body RequestBody token
+    );
+}
