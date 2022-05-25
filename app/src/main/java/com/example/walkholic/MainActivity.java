b0c045e587ba5174577dcdb52923a546779bed01
Haechan Lee <dlgockssla@gmail.com>
2022-05-25 18:09:47 +0900

    final

--- app/src/main/java/com/example/walkholic/MainActivity.java
+++ app/src/main/java/com/example/walkholic/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.walkholic;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
 import com.example.walkholic.Service.ServerRequestApi;
 
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,7 +35,11 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener , SensorEventListener {
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
 
     Button btn_home;
     Button btn_search;
@@ -42,13 +56,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn_logout;
     Button btn_revoke;
 
+    SensorManager sensorManager;
+    Sensor stepCountSensor;
+    TextView stepCountView;
+    TextView cals;
+    ProgressBar progBar;
+    // 현재 걸음 수
+    int currentSteps;
+    double tt = 0;
+
     private ServerRequestApi testService; // Service 요청 변수
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        stepCountView = findViewById(R.id.stepCountView);
+        progBar = findViewById(R.id.progressBar1);
+        currentSteps = preferences.getInt("step", 0);
+        stepCountView.setText(String.valueOf(currentSteps) + " / 10000 steps");
+        tt = (int) (currentSteps * 0.035);
+        cals.setText(String.valueOf(tt) + " kcal");
+        Log.d("asdasd", "onResume");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progBar = findViewById(R.id.progressBar1);
+        stepCountView = findViewById(R.id.stepCountView);
+
+        cals = findViewById(R.id.cals);
+
+        //기본 SharedPreferences 환경과 관련된 객체를 얻어옵니다.
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        // SharedPreferences 수정을 위한 Editor 객체를 얻어옵니다.
+        editor = preferences.edit();
+        currentSteps = preferences.getInt("step", 0);
+        progBar.setProgress((int) currentSteps / 10);
+
+        // 활동 퍼미션 체크
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED) {
+
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        // 걸음 센서 연결
+        // * 옵션
+        // - TYPE_STEP_DETECTOR:  리턴 값이 무조건 1, 앱이 종료되면 다시 0부터 시작
+        // - TYPE_STEP_COUNTER : 앱 종료와 관계없이 계속 기존의 값을 가지고 있다가 1씩 증가한 값을 리턴
+        //
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        // 디바이스에 걸음 센서의 존재 여부 체크
+        if (stepCountSensor == null) {
+            Toast.makeText(this, "No Step Sensor", Toast.LENGTH_SHORT).show();
+            Log.d("asdasd", "sensor1");
+        } else {
+            Log.d("asdasd", "sensor2");
+        }
+
         context = this; // 이해찬 추가
         auth = FirebaseAuth.getInstance();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -117,6 +190,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+
+    }
+
+
+    public void onStart() {
+        super.onStart();
+        if (stepCountSensor != null) {
+            // 센서 속도 설정
+            // * 옵션
+            // - SENSOR_DELAY_NORMAL: 20,000 초 딜레이
+            // - SENSOR_DELAY_UI: 6,000 초 딜레이
+            // - SENSOR_DELAY_GAME: 20,000 초 딜레이
+            // - SENSOR_DELAY_FASTEST: 딜레이 없음
+            //
+            // Log.d("asdasd", "sensor3");
+            sensorManager.registerListener(this, stepCountSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // 걸음 센서 이벤트 발생시
+        if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            if (event.values[0] == 1.0f) {
+                // 센서 이벤트가 발생할때 마다 걸음수 증가
+                currentSteps++;
+                editor.putInt("step", currentSteps);
+                editor.commit();
+                tt = (int) (currentSteps * 0.035);
+                stepCountView.setText(String.valueOf(currentSteps) + " / 10000 steps");
+                progBar.setProgress((int) currentSteps / 10);
+                cals.setText(String.valueOf(tt) + " kcal");
+            }
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
